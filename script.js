9cc470d10644aa7d9d7437b43bad3a74de955925
KhrlFbrn <febrianakhoirul280202@gmail.com>
2023-05-05 19:18:27 +0700

    update firebase

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {databaseURL: "https://regal-cider-353012-default-rtdb.asia-southeast1.firebasedatabase.app"}
-//   apiKey: "AIzaSyBszCHEz1MUtnC4k6E3-AP6mgC2I9DM5xk",
-//   authDomain: "regal-cider-353012.firebaseapp.com",
-//   projectId: "regal-cider-353012",
-//   storageBucket: "regal-cider-353012.appspot.com",
-//   messagingSenderId: "1033711011401",
-//   appId: "1:1033711011401:web:bd84bb3ee7555cde9e6198",
-//   measurementId: "G-D1T5BPV1T1"
+const firebaseConfig = {databaseURL: "https://regal-cider-353012-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: "AIzaSyBszCHEz1MUtnC4k6E3-AP6mgC2I9DM5xk",
+  authDomain: "regal-cider-353012.firebaseapp.com",
+  projectId: "regal-cider-353012",
+  storageBucket: "regal-cider-353012.appspot.com",
+  messagingSenderId: "1033711011401",
+  appId: "1:1033711011401:web:bd84bb3ee7555cde9e6198",
+  measurementId: "G-D1T5BPV1T1"}
 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 const database = getDatabase(app)
 const inputDB = ref(database, "follow")
 
